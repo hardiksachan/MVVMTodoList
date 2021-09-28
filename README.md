b6038f8036dcef153fdee2a8093e686e64d6e268
Hardik Sachan <sachanhardik@gmail.com>
2021-09-28 09:53:29 +0530

    README added

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1 align="center">Todo List App</h1>
+
+<p align="center">An android Todo List app</p>
+
+I am creating this project to teach myself about SQLDelight, Dependency Injection using Hilt, and Android Architecture Components.
+
+I'll be following Coding In Flow's [tutorial](https://www.youtube.com/playlist?list=PLrnPJCHvNZuCfAe7QK2BoMPkv2TGM_b0E), but there are some changes in tech stack used.
+
+## Built With
+
+- [Jetpack Compose](https://developer.android.com/jetpack/compose)
+- [SQLDelight](https://cashapp.github.io/sqldelight/)
+- [Jetpack DataStore](https://developer.android.com/topic/libraries/architecture/datastore)
+- Android Studio
+- Kotlin
+
+## Author
+
+**Hardik Sachan**
+
+- [Github](https://github.com/hardiksachan)
+- [LinkedIn](https://www.linkedin.com/in/hardik-sachan/)
+- [Twitter](https://twitter.com/simplyHardikk)
+
+## 🤝 Support
+
+Give a ⭐️ if you like this project!
--- app/src/main/java/com/hardiksachan/mvvmtodolist/MainActivity.kt
+++ app/src/main/java/com/hardiksachan/mvvmtodolist/MainActivity.kt
@@ -8,15 +8,14 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModel
-import com.hardiksachan.mvvmtodolist.ui.theme.MVVMTodoListTheme
+import com.hardiksachan.mvvmtodolist.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MVVMTodoListTheme {
+            AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
@@ -34,7 +33,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MVVMTodoListTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
